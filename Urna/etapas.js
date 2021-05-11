166f70162aceae5c7568ac5887f1acdad86be254
Gabriel Contesini <gabrielct.moraes@gmail.com>
2021-05-11 17:11:47 -0300

    First commit

---
+++ Urna/etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+  {
+    titulo: 'VEREADOR',
+    numeros: 5,
+    candidatos: [
+      {
+        numero: '38111',
+        nome: 'Cleiton dos Santos',
+        partido: 'ABC',
+        fotos:[
+          {url: '38111.jpg', legenda:'Vereador'}
+        ]
+      },
+      {
+        numero: '77222',
+        nome:'Rosangela das Neves',
+        partido: 'DEFG',
+        fotos:[
+          {url: '77222.jpg', legenda: 'Vereador'}
+        ]
+      },
+    ]
+  },
+  {
+    titulo: 'PREFEITO',
+    numeros: 2,
+    candidatos: [
+      {
+        numero: '99',
+        nome:'Reinaldo Silva',
+        partido: 'ABC',
+        vice: 'Torres Bwoskoski',
+        fotos:[
+          {url: '99.jpg', legenda: 'Prefeito'},
+          {url: '99_2.jpg', legenda: 'Vice-Prefeito', small: true}
+        ]
+      },
+      {
+        numero: '84',
+        nome: 'Elisangela Torres',
+        partido: 'QWERTY',
+        vice: 'João Raimundo',
+        fotos:[
+          {url: '84.jpg', legenda:'Prefeito'},
+          {url: '84_2.jpg', legenda:'Vice-Prefeito', small: true}
+        ]
+      },
+    ]
+  }
+];
